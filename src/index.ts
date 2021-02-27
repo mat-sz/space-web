dfc8b1c0186aff5bb9e778461f2e7bbe35fd310a
Mat Sz <contact@matsz.dev>
2021-02-27 13:17:49 +0100

    lowered amount of rendered meshes

--- src/index.ts
+++ src/index.ts
@@ -24,17 +24,11 @@ moveInput.keysDown = [83];
 moveInput.keysLeft = [65];
 moveInput.keysRight = [68];
 
-const camera = new FreeCamera('playerCamera', new Vector3(0, 1, 0), scene);
+const camera = new FreeCamera('playerCamera', new Vector3(8, 8, 8), scene);
 camera.setTarget(Vector3.Zero());
 camera.inputs.removeByType('FreeCameraKeyboardMoveInput');
 camera.inputs.add(moveInput);
 camera.attachControl(canvas, true);
-
-const playerBox = Mesh.CreateBox('player', 1, scene);
-playerBox.position = new Vector3(10.5, 34.5, 10.5);
-playerBox.material = material;
-
-camera.parent = playerBox;
 
 const light = new HemisphericLight('light1', new Vector3(0, 35, 0), scene);
 light.intensity = 10;
@@ -51,9 +45,9 @@ const map = new Array(32)
       )
   );
 
-for (let x = 0; x < 32; x++) {
-  for (let y = 0; y < 32; y++) {
-    for (let z = 0; z < 32; z++) {
+for (let x = 0; x < 16; x++) {
+  for (let y = 0; y < 16; y++) {
+    for (let z = 0; z < 16; z++) {
       if (map[x][y][z]) {
         const box = Mesh.CreateBox('box', 0.8, scene);
         box.position = new Vector3(x, y, z);
